f111c5f4c41d71687a242717f319803d81c4a484
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2021-12-15 17:31:10 +0100

    inv -e everywhere

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -30,8 +30,8 @@ inv -e install-tools
 inv -e deps
 
 # Run rtloader test
-inv rtloader.make --install-prefix=$GOPATH/src/github.com/DataDog/datadog-agent/dev
-inv rtloader.install
+inv -e rtloader.make --install-prefix=$GOPATH/src/github.com/DataDog/datadog-agent/dev
+inv -e rtloader.install
 inv -e rtloader.test
 
 # Run unit tests
