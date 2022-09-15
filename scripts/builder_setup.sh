07b71e54af3981fc1bfbc6213a3e4b1213b6476b
Dustin Mitchell <dustin.mitchell@datadoghq.com>
2022-09-15 12:49:35 -0400

    Revert early landing of "Upgrade to go1.18.6 (7.39.x) (#110)"
    This reverts commit e872ce46815d23f1cfe784b2322b7956f39b00ce.

--- scripts/builder_setup.sh
+++ scripts/builder_setup.sh
@@ -25,7 +25,7 @@ export PYTHON_VERSION=3.8.11
 export CMAKE_VERSION=3.18.2.2
 export GIMME_VERSION=1.5.4
 
-export GO_VERSION=1.18.6
+export GO_VERSION=1.17.11
 # Newer version of IBM_MQ have a different name
 export IBM_MQ_VERSION=9.2.4.0-IBM-MQ-DevToolkit
 #export IBM_MQ_VERSION=9.2.2.0-IBM-MQ-Toolkit
