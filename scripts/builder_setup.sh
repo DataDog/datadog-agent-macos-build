2b391241b2bdae70e884a1934c8f860e883ceb0a
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-07-03 18:11:15 +0200

    fix(builder_setup): better retry message

--- scripts/builder_setup.sh
+++ scripts/builder_setup.sh
@@ -56,7 +56,7 @@ unset HOMEBREW_NO_INSTALL_FROM_API
 HOMEBREW_CLONE_RETRY_NB=3
 
 for i in $(seq 0 $HOMEBREW_CLONE_RETRY_NB); do
-    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || echo "Clone failed !! Retrying in 5 sec..."
+    CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || echo "Homebrew installation failed !! Retrying in 5 sec..."
     if [ $? = 0 ]; then
         break
     fi
