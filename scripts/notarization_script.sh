078a800a60712d7f68cae013880dbbb0df7159a5
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-07-26 14:22:42 +0200

    notarization: update from atool to notarytool [AP-2169]
    Most of the code can go away since the new notarytool supports a --wait
    option which prevents us from having to poll the status

--- scripts/notarization_script.sh
+++ scripts/notarization_script.sh
@@ -60,68 +60,6 @@ echo "Sending notarization request."
 #         <string>4.00.1181</string>
 # </dict>
 # </plist>
-for i in {1..5}; do
-    NOTARIZATION_REQUEST_OUTPUT=$(xcrun altool --notarize-app --primary-bundle-id "com.datadoghq.agent.$RELEASE_VERSION" --username "$APPLE_ACCOUNT" --password "$NOTARIZATION_PWD" --file "$LATEST_DMG" --output-format xml)
-    echo "$NOTARIZATION_REQUEST_OUTPUT"
-    REQUEST_UUID=$(echo "$NOTARIZATION_REQUEST_OUTPUT" | xpath -e "/plist/dict/key[text()='notarization-upload']/following-sibling::*[1]/key[text()='RequestUUID']/following-sibling::*[1]/text()")
-    if [[ -n "$REQUEST_UUID" ]]; then
-        break
-    fi
 
-    sleep 5
-done
+xcrun altool submit --apple-id "$APPLE_ACCOUNT" --password "$NOTARIZATION_PWD" "$LATEST_DMG" --wait
 
-if [[ -z "$REQUEST_UUID" ]]; then
-    echo "Error while retrieving REQUEST_UUID."
-    exit 1
-fi
-
-# Example notarization status output:
-# <?xml version="1.0" encoding="UTF-8"?>
-# <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-# <plist version="1.0">
-# <dict>
-#         <key>notarization-info</key>
-#         <dict>
-#                 <key>Date</key>
-#                 <date>2020-08-25T17:48:58Z</date>
-#                 <key>Hash</key>
-#                 <string>12e79ec...</string>
-#                 <key>LogFileURL</key>
-#                 <string>https://osxapps-ssl.itunes.apple.com/itunes-assets/...</string>
-#                 <key>RequestUUID</key>
-#                 <string>wwwwwwww-xxxx-yyyy-zzzz-tttttttttttttttt</string>
-#                 <key>Status</key>
-#                 <string>invalid</string>
-#                 <key>Status Code</key>
-#                 <integer>2</integer>
-#                 <key>Status Message</key>
-#                 <string>Package Invalid</string>
-#         </dict>
-#         <key>os-version</key>
-#         <string>10.14.6</string>
-#         <key>success-message</key>
-#         <string>No errors getting notarization info.</string>
-#         <key>tool-path</key>
-#         <string>/Applications/Xcode.app/Contents/SharedFrameworks/ContentDeliveryServices.framework/Versions/A/Frameworks/AppStoreService.framework</string>
-#         <key>tool-version</key>
-#         <string>4.00.1181</string>
-# </dict>
-# </plist>
-
-echo "Waiting for notarization process to complete."
-while [[ -z "$NOTARIZATION_STATUS_CODE" ]]; do
-    sleep 30
-    echo "Fetching notarization status"
-    NOTARIZATION_STATUS_OUTPUT=$(xcrun altool --notarization-info "$REQUEST_UUID" -u "$APPLE_ACCOUNT" -p "$NOTARIZATION_PWD" --output-format xml)
-    echo "$NOTARIZATION_STATUS_OUTPUT"
-    NOTARIZATION_STATUS_CODE=$(echo "$NOTARIZATION_STATUS_OUTPUT" | xpath -e "/plist/dict/key[text()='notarization-info']/following-sibling::*[1]/key[text()='Status Code']/following-sibling::*[1]/text()")
-done
-
-# Print final status in the console
-xcrun altool --notarization-info "$REQUEST_UUID" -u "$APPLE_ACCOUNT" -p "$NOTARIZATION_PWD"
-
-if [[ "$NOTARIZATION_STATUS_CODE" -ne "0" ]]; then
-    exit 1
-fi
-exit 0
