a5e753993cdf2ffa23713b5163354e5e1f3d4914
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-11-14 14:01:51 +0100

    CI: add a new linting workflow [APL-2393]
    This currently duplicates the linting phase until the workflow exists on main as
    well, at which point we can trigger it from datadog-agent and remove the linting
    from the test script

---
+++ scripts/lint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/)
+# Copyright 2022-present Datadog, Inc.
+
+set -e
+
+# Fetches the datadog-agent repo, checks out to the requested version
+# and lints the code
+
+# Prerequisites:
+# - clone_agent.sh has been run
+# - builder_setup.sh has been run
+# - $VERSION contains the datadog-agent git ref to target
+# - $PYTHON_RUNTIMES contains the included python runtimes
+
+# Load build setup vars
+source ~/.build_setup
+cd $GOPATH/src/github.com/DataDog/datadog-agent
+
+# Install python deps (invoke, etc.)
+python3 -m pip install -r requirements.txt
+
+# Install dependencies
+inv -e install-tools
+inv -e deps
+
+# Run go linters
+inv -e lint-go --cpus 3
+
