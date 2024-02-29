b2c532447a9f5c717e34f32624226f16fae4ad4a
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-02-29 13:36:26 +0100

    Introduce workflow variable to enable fast unit tests

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -34,8 +34,10 @@ inv -e rtloader.install
 # FIXME: rtloader tests fail on Mac with "image not found" errors
 #inv -e rtloader.test
 
+FAST_TESTS_FLAG=""
+if [ "$FAST_TESTS" = "true" ]; then FAST_TESTS_FLAG="--only-impacted-packages"; fi
 # Run unit tests
-inv -e test --rerun-fails=2 --python-runtimes $PYTHON_RUNTIMES --race --profile --cpus 3 --save-result-json "test_output.json" --junit-tar "junit-tests_macos.tgz"
+inv -e test --rerun-fails=2 --python-runtimes $PYTHON_RUNTIMES --race --profile --cpus 3 --save-result-json "test_output.json" --junit-tar "junit-tests_macos.tgz" $FAST_TESTS_FLAG
 
 # Run invoke task tests
 inv -e invoke-unit-tests
