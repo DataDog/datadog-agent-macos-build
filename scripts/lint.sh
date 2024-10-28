008cecd453c8a86ab5f4a6f7c626a5f8e78eb4b9
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-10-28 10:39:29 +0100

    Revert "Revert "drop python2 support (#213)" (#214)" (#219)
    This reverts commit 8fb0ae50828f880a5e58ac92fde46f9caaca08f4.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -14,7 +14,6 @@ set -e
 # - clone_agent.sh has been run
 # - builder_setup.sh has been run
 # - $VERSION contains the datadog-agent git ref to target
-# - $PYTHON_RUNTIMES contains the included python runtimes
 
 # Load build setup vars
 source ~/.build_setup
